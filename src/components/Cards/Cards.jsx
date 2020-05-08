7dc04780b45a9b5ed30ea2c0c872db298613547e
sujitraul <sraul111@gmail.com>
2020-05-09 00:32:55 +0530

    add css for cards component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import styles from './Cards.module.css';
+import cx from 'classnames'
 import CountUp from 'react-countup';
+
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     if (!confirmed) {
@@ -10,7 +12,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card} >
+                <Grid item component={Card} xs={12} md= {3} className={cx(styles.card,styles.infected)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
@@ -20,7 +22,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card} >
+                <Grid item component={Card} xs={12} md= {3} className={cx(styles.card,styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
@@ -30,7 +32,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography variant="body2">Number of recoveries due to COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card} >
+                <Grid item component={Card} xs={12} md= {3} className={cx(styles.card,styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">
@@ -42,6 +44,8 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                 </Grid>
 
             </Grid>
+
+
         </div>
     )
 }
