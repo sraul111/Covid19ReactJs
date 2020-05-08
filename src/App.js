7c1aaf4a16b59437f37ee7512a54c47135e483c8
sujitraul <sraul111@gmail.com>
2020-05-08 23:25:23 +0530

    add material UI and used it in cards

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import React from 'react';
 // import CountryPicker from './components/CountryPicker/CountryPicker'
 import {Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
+import {fetchdata} from './api'
+
 class App extends React.Component
 {
+    state = {
+        data : {},
+    }
+    async componentDidMount(){
+        const fetcheddata =  await fetchdata();
+        this.setState({data:fetcheddata});
+    }
+
      render(){
+         const { data } = this.state;
+    
          return (
              <div className={styles.container}>
-                 <Cards/>
+                 <Cards data = { data }/>
                  <Chart/>
                  <CountryPicker/>
              </div>
